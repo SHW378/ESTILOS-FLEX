9d6520a44302d112b71770805d425ec6b8c4bde5
SHW378 <cesarbrmn@hotmail.com>
2025-04-28 18:40:56 -0600

    js

---
+++ js/myFunction.js
@@ -0,0 +1,10 @@
+
+let numero = 5;
+
+let numero2 = 6;
+
+let resultado = numero + numero2;
+
+console.log("El resultado de sumar " + numero + " y " + numero2 + " es: " + resultado);
+
+alert("El resultado es: " + resultado);
