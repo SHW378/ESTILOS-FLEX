ae68091a43f4e6e942979df0171a30ce6fca29d0
SHW378 <cesarbrmn@hotmail.com>
2025-05-05 18:35:54 -0600

    arreglo de objetos

--- js/ejemploFor.js
+++ js/ejemploFor.js
@@ -1,6 +1,24 @@
 // declarar una estructura de datos con nuestra información 
 let productos = ["producto 1 ", "producto 2 ", "producto 3 ", "producto 4 ", "producto 5 ", "producto 6 ", "producto 7 ", "producto 8 ", "producto 9 ", "producto 10 "];
 
+//Arreglos de objetos
+let products = [
+    {   nombreProducto: "Producto1",
+        descripcion: "Este es el producto 1",
+    },
+    {   nombreProducto: "Producto2",
+        descripcion: "Este es el producto 2",
+    },
+    {   nombreProducto: "Producto3",
+        descripcion: "Este es el producto 3",
+    },
+    {   nombreProducto: "Producto4",
+        descripcion: "Este es el producto 4",
+    },
+    {   nombreProducto: "Producto5",
+        descripcion: "Este es el producto 5",
+    }
+];
 //Accedo al elemento de html que quiero alterar
 let contenedor = document.getElementById("contenedor"); 
 
