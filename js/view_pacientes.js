2c3f49e83cdac09d4e321ed77f33fb5544b053e4
SHW378 <cesarbrmn@hotmail.com>
2025-05-14 18:01:08 -0600

    funcion

---
+++ js/view_pacientes.js
@@ -0,0 +1,41 @@
+//Declaramos nuestra estructura de datos a implementar
+
+let pacientes = [
+    {   id: 1,
+        nombre: "Cristian",
+        apellido: "Estrada Andrade",
+        fechaNacimiento: "233-11-1997",
+        genero: "Masculino"
+    },
+    {   id: 2,
+        nombre: "César",
+        apellido: "Cázares Chaván",
+        fechaNacimiento: "17-12-2005",
+        genero: "Masculino"
+        
+    },
+    {   id: 3,
+        nombre: "Victor",
+        apellido: "Chavéz Valadez",
+        fechaNacimiento: "10-01-2006",
+        genero: "Masculino"
+    }
+];
+
+//Funciones en javascript = Metodos de Java
+/* 
+function sumar(num1, num2) {
+
+}
+*/
+
+//Función para cargar los registros en la tabla
+function mostrarTabla() {
+    let tabla = document.getElementById("tablaPacientes");
+    tabla.innerHTML = ""; //Limpiamos los registor actuales de la tabla
+    
+    let cuerpoTabla = "";
+    pacientes.forEach(paciente =>{
+        cuerpoTabla += ``;
+    });
+}
